1a324cddf422e389cc019ee9d0768e08a2df49c5
Sona Hasanova <hesenovasonaa@gmail.com>
2026-08-13 19:02:02 +0400

    Create responsive page

---
+++ script.js
@@ -0,0 +1,6 @@
+const burger = document.getElementById("burger");
+    const nav = document.querySelector(".nav");
+    burger.addEventListener("click", () => {
+        burger.classList.toggle("active");
+        nav.classList.toggle("active");
+    });
